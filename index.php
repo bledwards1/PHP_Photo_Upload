29ddd7b964b7e34d81c96de03c06931bc0990b3b
Brad Edwards <bledwards1@gmail.com>
2016-01-18 18:26:03 -0600

    index.php
    This is a bootstrap powered photo upload script. It has very basic validation checks.

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <title>Upload Image</title>
+<style type="text/css">
+@import url("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
+</style>
+
+<?php if($_SERVER['REQUEST_METHOD'] == 'GET') { ?>
+<form method="post" action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" enctype="multipart/form-data">
+      <div class="col-sm-6 col-md-4">
+<div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Upload Image...</h3>
+          </div>
+            <div class="panel-body">
+                      <div class="row">
+                  					<div  class="form-group col-sm-5">
+        </div>
+        </div>
+              Select image to upload:
+              <input class="btn btn-default" type="file" name="fileToUpload" id="fileToUpload">
+              <br/>
+              <input class="btn btn-default" type="submit" value="Upload Image" name="submit">         
+            </div>
+        </div>
+  </div>
+  
+</form>
+<?php 
+ } else {
+  $uploadOk = 1;  
+  if (isset($_FILES['fileToUpload']) &&
+      ($_FILES['fileToUpload']['error'] == UPLOAD_ERR_OK)) {
+      $newPath = '/tmp/' . basename($_FILES['fileToUpload']['name']);
+      $imageFileType = pathinfo($newPath,PATHINFO_EXTENSION);
+      // Check if file already exists
+      if (file_exists($newPath)) {
+          echo "Sorry, file already exists.";
+          $uploadOk = 0;
+      }   
+      // Check file size
+      if ($_FILES["fileToUpload"]["size"] > 500000) {
+          echo "Sorry, your file is too large.";
+          $uploadOk = 0;
+      }
+      // Allow certain file formats
+      if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+          echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+          $uploadOk = 0;
+      }
+     // *** End of checks ***
+    
+      if ($uploadOk == 1) {
+        move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $newPath);
+        print "Success! The photo was uploaded";
+      } else {
+        print "<br/>Photo not uploaded.";
+      }
+    }
+    else {
+      print "<br/>No valid file uploaded.";
+  }
+ }
+?>
